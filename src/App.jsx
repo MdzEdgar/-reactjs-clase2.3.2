6e9b94ebaa2aa7f022d4f494f3cf069309b9ecc1
Edgar Méndez <mdzredgar@gmail.com>
2023-01-25 21:38:00 -0600

    Aplicación de Noticias con renderizado oficial

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,43 @@ import CardNew from './components/CardNew'
 function App() {
 
   const [news, setNews] = useState([])
+  const [nameNew, setNameNew] = useState("")
 
   const getNews = () => {
-    const URL = "https://newsapi.org/v2/top-headlines?q=méxico&language=es&apiKey=88e561158bbe427a94227d99351334c2"
+    const URL = `https://newsapi.org/v2/top-headlines?q=${nameNew}&language=es&apiKey=88e561158bbe427a94227d99351334c2`
     axios.get(URL)
     .then((res) => {setNews(res.data.articles)})
     .catch((err) => {console.log(err)})
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNameNew(e.target.textNew.value)
+  }
+
   useEffect(() => {
     getNews()
-  }, [])
+  }, [nameNew])
 
   return (
     <div className="App">
-      <h1>Good news Academlo!</h1>
-      <form>
-        <div>
-          <label htmlFor=""></label>
-          <input type="text" />
+      <h1 className='App__title'>Good news Academlo!</h1>
+      <form className='App__form' onSubmit={handleSubmit}>
+        <div className='App__form-div'>
+          <input className='App__form-input' id="textNew" placeholder='Busca tu noticia...' type="text" />
         </div>
-        <button>Search</button>
+        <button className='App__form-btn'>Search</button>
       </form>
-      <section>
-        <CardNew newNote={news[0]} />
-        <CardNew newNote={news[1]} />
-        <CardNew newNote={news[2]} />
+      <section className='container-articles'>
+        {
+          news[0] && <CardNew newNote={news[0]} /> 
+        }
+        {
+          news[1] && <CardNew newNote={news[1]} /> 
+        }
+        {
+          news[2] && <CardNew newNote={news[2]} /> 
+        }
       </section>
     </div>
   )
--- src/components/CardNew.jsx
+++ src/components/CardNew.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const CardNew = ({newNote}) => {
   return (
-    <article>
-      <h3>{newNote?.title}</h3>
-      <p>{newNote?.description}</p>
-      <h6>{newNote?.publishedAt}</h6>
+    <article className='article'>
+      <div className='article__img'>
+        <img src={newNote?.urlToImage} alt={newNote?.title} />
+      </div>
+      <h3 className='article__title'>{newNote?.title}</h3>
+      <p className='article__description'>{newNote?.description}</p>
+      <h6 className='article__publishedDate'>{newNote?.publishedAt}</h6>
+      <a className='article__a' target='_blank' href={newNote?.url}>Más información</a>
     </article>
   )
 }
